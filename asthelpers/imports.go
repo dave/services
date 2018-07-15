6de722888420c732b71c679460e37a4cbf8b6c9d
Dave Brophy <dave@brophy.uk>
2018-07-15 20:38:01 +0700

    Add init values to NewPackageSession

---
+++ asthelpers/imports.go
@@ -0,0 +1,40 @@
+package asthelpers
+
+import (
+	"go/ast"
+	"go/token"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+func RefreshImports(f *ast.File) {
+
+	// Delete all imports from the ast tree
+	astutil.Apply(f, func(c *astutil.Cursor) bool {
+		switch n := c.Node().(type) {
+		case *ast.GenDecl:
+			if n.Tok == token.IMPORT {
+				c.Delete()
+			}
+		case *ast.ImportSpec:
+			c.Delete()
+		}
+		return true
+	}, nil)
+
+	// Add them again from f.Imports
+	if len(f.Imports) > 0 {
+		var importSpecs []ast.Spec
+		for _, is := range f.Imports {
+			importSpecs = append(importSpecs, is)
+		}
+		importGenDecl := &ast.GenDecl{
+			Tok:    token.IMPORT,
+			Lparen: token.Pos(1), // must be non-zero to render as a list
+			Specs:  importSpecs,
+			Rparen: token.Pos(1), // must be non-zero to render as a list
+		}
+		f.Decls = append([]ast.Decl{importGenDecl}, f.Decls...)
+	}
+
+}
